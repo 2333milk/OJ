a4fa63642febe5c91ce09b5a7c674a8a104ea5c5
张丽星 <3143806613@qq.com>
2024-06-16 11:36:34 +0800

    createProject

---
+++ nchuOJ-backend/src/main/java/com/jueye/nchuOJbackend/NchuOjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jueye.nchuOJbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NchuOjBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NchuOjBackendApplication.class, args);
+    }
+
+}
